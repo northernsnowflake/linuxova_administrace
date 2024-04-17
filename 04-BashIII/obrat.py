b582d537b6177a5e73754df31cbde42cedc497c2
Kateřina Chmelařová <chmelarova.kata@gmail.com>
2024-04-17 15:05:54 +0200

    functional program, all in the assignment done

--- 04-BashIII/obrat.py
+++ 04-BashIII/obrat.py
@@ -1,7 +1,4 @@
-# ve složce ./bin
-
-
-! /usr/bin/python3
+#!/usr/bin/python3
 
 import sys
 import os
@@ -22,44 +19,47 @@ obracena_pismena = {
 }
 
 def obrat(text):
+    for c in text:
+        if c not in obracena_pismena:
+            sys.stdout.write(''.join(obracena_pismena.get(c, c) for c in reversed(text)))
+            sys.stdout.write('\n')
+            exit(2)
     return ''.join(obracena_pismena.get(c, c) for c in reversed(text))
 
 
-#for line in sys.stdin:
-#print(obrat(line.rstrip()))
-
-#sys.argv
-
-if "-" in opts:
-    for line in sys.stdin:
-       print(obrat(line.rstrip()))
-
-if sys.argv == '-':
-    for line in sys.stdin:
-        print(obrat(line.rstrip()))
-
-
-if len(sys.argv) > 1:
+#zpracovani argumentu "-", je-li na prvni pozici
+if sys.argv[1:] == '-':
+        for line in sys.stdin:
+            print(obrat(line.rstrip()))
+#zpracovani argumentu '-','--help' a pokud prepinac neexistuje
+elif len(sys.argv) > 1:
     for jmeno in sys.argv[1:]:
-        if jmeno == '-':
-            for line in sys.stdin:
-                print(obrat(line.rstrip()))
         try:
-            with open(jmeno,"r") as file:
-                obsah = file.read()
-                #for line in obsah:
-                slova = obsah.split()
-                for slovo in slova:
-                    print(obrat(slovo))
+            if jmeno == '-':
+                for line in sys.stdin:
+                    print(obrat(line.rstrip()))
+            elif jmeno == "--help":
+                print(f"Napoveda: ")
+                print(f"-       Standardni vstup")
+                print(f"name    Je-li zadano jmeno souboru, zpracuje tento soubor")
+                break
+            elif jmeno[0] == "-" and len(jmeno) > 1 and jmeno != "--help":
+                sys.stderr.write(f"Prepinac {jmeno} neexistuje, viz --help\n")
+                exit(1)
+                break
+            else:
+                with open(jmeno,"r") as file:
+                    obsah = file.read()
+                    #for line in obsah:
+                    slova = obsah.split()
+                    for slovo in slova:
+                        print(obrat(slovo))
         except FileNotFoundError:
             print(f"File not found")
 else:
     for line in sys.stdin:
         print(obrat(line.rstrip()))
 
-
-#opts = [opt for opt in sys.argv[1:] if opt.startswith('-')]
-#args = [arg for arg in sys.argv[1:] if not arg.startswith('-')]
 
 
                                             
